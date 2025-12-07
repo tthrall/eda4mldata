37da42ad069e01dda75ca7ed59ab345b08cff2e7
tthrall <tthrall@Alumni.Stanford.edu>
2025-12-07 12:52:19 +0000

    Include dry_beans (from beans::beans).

---
+++ data-raw/dry_beans.R
@@ -0,0 +1,40 @@
+##
+# dry_beans.R
+#   Prepare dry beans dataset for eda4mldata package
+##
+
+# Load beans data from beans package
+dry_beans <- beans::beans |>
+  tibble::as_tibble() |>
+  dplyr::rename(
+    area             = area,
+    perimeter        = perimeter,
+    major_axis       = major_axis_length,
+    minor_axis       = minor_axis_length,
+    aspect_ratio     = aspect_ratio,
+    eccentricity     = eccentricity,
+    convex_area      = convex_area,
+    equiv_diameter   = equiv_diameter,
+    extent           = extent,
+    solidity         = solidity,
+    roundness        = roundness,
+    compactness      = compactness,
+    shape_factor_1   = shape_factor_1,
+    shape_factor_2   = shape_factor_2,
+    shape_factor_3   = shape_factor_3,
+    shape_factor_4   = shape_factor_4,
+    class            = class
+  ) |>
+  dplyr::mutate(
+    class = tolower(class)
+  )
+
+# Verify: should be 13,611 beans
+stopifnot(nrow(dry_beans) == 13611L)
+
+# Save to package data directory
+usethis::use_data(dry_beans, overwrite = TRUE)
+
+##
+#  EOF
+##
